4ca37c96711ced1a1ef53d5130507e8ff8692529
SergiSM7 <65180552+SergiSM7@users.noreply.github.com>
2021-02-22 18:09:09 +0100

    Add files via upload

--- js/moviments.js
+++ js/moviments.js
@@ -1,64 +1,56 @@
+let preguntaEnCurs = 1;
+let preguntaUltima = 4;
+
 function passaSeguent(objRebut){
-    //let idObjPare = document.getElementById(objRebut.parentElement.id);
-    //let ObjPare = document.getElementById(idObjPregAct);
-    let idObjPregAct = objRebut.parentElement.id;
-    let ObjPregAct = document.getElementById(idObjPregAct);
-    let colarticles = document.getElementsByTagName("article");
+
+  if (preguntaEnCurs == preguntaUltima){
+    // alert("Ets a la darrera pregunta!" + preguntaEnCurs + " de " + preguntaUltima);
+    preguntaEnCurs --;
+  } else {
+    // alert("Ets a la pregunta " + preguntaEnCurs + " de " + preguntaUltima);
+    let idObjPregAct = objRebut.parentElement.parentElement.id; 
+    let colArticles = document.getElementsByTagName("article");
     let idObjPregSeg;
-    // let ultimaPreg = false;
-    
-    for (let i = 0; i < colarticles.length; i++) {
-        if (colarticles[i].id == idObjPregAct){
-            idObjPregSeg = colarticles[i+1].id;
-            break;
-        };
+    for (let i = 0; i < colArticles.length; i++) {
+      if(colArticles[i].id == idObjPregAct){
+        idObjPregSeg = colArticles[i+1].id;
+        break;
+      };
     }
     
-    // ObjPregSeg = document.getElementById(idObjPregSeg);
-    // ObjPregAct.classList.remove("elementVisible");
-    // ObjPregAct.classList.add("elementOcult");
-    // ObjPregSeg.classList.remove("elementOcult");
-    // ObjPregSeg.classList.add("elementVisible");
-
     amagaElement(idObjPregAct);
     mostraElement(idObjPregSeg);
+    preguntaEnCurs++;
+  }
+
 
 }
+
 
 function passaAnterior(objRebut){
-    //let idObjPare = document.getElementById(objRebut.parentElement.id);
-    //let ObjPare = document.getElementById(idObjPregAct);
-    let idObjPregAct = objRebut.parentElement.id;
-    let ObjPregAct = document.getElementById(idObjPregAct);
-    let colarticles = document.getElementsByTagName("article");
-    let idObjPregAnt;
-    // let ultimaPreg = false;
-    
-    for (let i = 0; i < colarticles.length; i++) {
-        if (colarticles[i].id == idObjPregAct){
-            idObjPregAnt = colarticles[i-1].id;
-            break;
-        };
-    }
-    
+//   alert("Ets a la pregunta " + preguntaEnCurs + " de " + preguntaUltima);
+  let idObjPregAct = objRebut.parentElement.parentElement.id; 
+  let colArticles = document.getElementsByTagName("article");
+  let idObjPregAnt;
+    for (let i = 0; i < colArticles.length; i++) {
+    if(colArticles[i].id == idObjPregAct){
+      idObjPregAnt = colArticles[i-1].id;
+      break;
+    };
+  }
+  
+  amagaElement(idObjPregAct);
+  mostraElement(idObjPregAnt);
+  preguntaEnCurs--;
 
-    // ObjPregAnt = document.getElementById(idObjPregAnt);
-    // ObjPregAct.classList.remove("elementVisible");
-    // ObjPregAct.classList.add("elementOcult");
-    // ObjPregAnt.classList.remove("elementOcult");
-    // ObjPregAnt.classList.add("elementVisible");
-
-    amagaElement(idObjPregAct);
-    mostraElement(idObjPregAnt);
-    
 }
 
-function mostraElement(idRebut) {
-    document.getElementById(idRebut).classList.add("elementVisible");
-    document.getElementById(idRebut).classList.remove("elementOcult");
+function mostraElement(idRebut){
+  document.getElementById(idRebut).classList.add("elementVisible");
+  document.getElementById(idRebut).classList.remove("elementOcult");
 }
 
-function amagaElement(idRebut) {
-    document.getElementById(idRebut).classList.remove("elementVisible");
-    document.getElementById(idRebut).classList.add("elementOcult");
+function amagaElement(idRebut){
+  document.getElementById(idRebut).classList.remove("elementVisible");
+  document.getElementById(idRebut).classList.add("elementOcult");
 }
